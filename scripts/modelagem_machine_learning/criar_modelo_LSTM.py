1ff5ec21a528faa2bad6b04e749ccb99b17bd759
CarolBw <carolbrescowitt@yahoo.com.br>
2025-04-11 22:40:15 +0000

    Incluindo Modelo LSTM

--- scripts/modelagem_machine_learning/criar_modelo_LSTM.py
+++ scripts/modelagem_machine_learning/criar_modelo_LSTM.py
@@ -1,21 +1,15 @@
 
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM, Dense, Dropout, Input
+from tensorflow.keras.layers import LSTM, Dense, Dropout, TimeDistributed
 
-def LSTM_model(input_shape, 
-               lstm_units=64, 
-               dropout_rate=0.2, 
-               optimizer='adam', 
-               loss='mse'):
-    """
-    Cria e retorna um modelo LSTM sequencial com parâmetros ajustáveis.
-    """
+def LSTM_model(input_shape):
     model = Sequential()
-    model.add(Input(shape=input_shape))
-    model.add(LSTM(lstm_units))
-    model.add(Dropout(dropout_rate))
-    model.add(Dense(1))
+    model.add(LSTM(units=64, return_sequences=True, input_shape=input_shape))
+    model.add(Dropout(0.2))
+    model.add(LSTM(units=32, return_sequences=True))
+    model.add(Dropout(0.2))
+    model.add(TimeDistributed(Dense(4)))  # 4 saídas por timestep
 
-    model.compile(loss=loss, optimizer=optimizer)
+    model.compile(optimizer='adam', loss='mse')
     return model
 
